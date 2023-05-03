d23d6aabd2a06322b09bfc0a5500f2aced9a57d2
Helen <59126064+Miyuki-L@users.noreply.github.com>
2023-05-03 13:25:09 -0700

    Update Image and audio source based on selection.

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,22 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  imageSelectionListner(); 
+}
+
+function imageSelectionListner() {
+  const selectElement = document.getElementById("horn-select");
+  const imgElement = document.querySelector('img');
+  const audioElement = document.getElementsByClassName("hidden");
+
+  selectElement.addEventListener("change", (event) => {
+    let value = event.target.value;
+    let imgSrc = `assets/images/${value}.svg`
+    let imgAlt = `${value.replace("-", " ")} image`;
+    let audioSrc = `assets/audio/${value}.mp3`;
+
+    imgElement.src = imgSrc;
+    imgElement.alt = imgAlt;
+    audioElement.src = audioSrc;
+  })
 }
