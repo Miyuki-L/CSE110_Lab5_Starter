97a591ae9f87bec0feba08a18ad03fe33b4ea65b
Helen <59126064+Miyuki-L@users.noreply.github.com>
2023-05-05 20:39:58 -0700

    Populated the select element with voices

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,25 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  populateVoiceList();
+}
+
+const synth = window.speechSynthesis;
+
+function populateVoiceList() {
+  synth.addEventListener("voiceschanged", ()=> {
+    const voiceSelect = document.querySelector("#voice-select")
+    const voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      option.setAttribute("index", i);
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+
+      voiceSelect.appendChild(option);
+    }    
+  })
 }
